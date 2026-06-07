1278a2cfba4f3f285a7de65bc2c67d4b20cc6127
ebordenave <edward.bordenave@gmail.com>
2026-06-07 05:17:16 -0400

    Lesson 09: AirHub Fixes

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,14 +1,30 @@
 //! Remove props parameter
 
+import Logoff from '../features/Logoff'
+import Logon from '../features/Logon'
 import useAuth from '../hooks/useAuth'
 
 //! Use useAuth() to access isAuthenticated
 
 function Header() {
   //! I don't have any props
-  // eslint-disable-next-line no-unused-vars
+
   const { isAuthenticated } = useAuth() //! why?
-  return <h1>Todo List</h1>
+  return (
+    <div>
+      {isAuthenticated ? (
+        <div>
+          <h1>Todo List</h1>
+          <Logoff />
+        </div>
+      ) : (
+        <div>
+          <h1>Todo List</h1>
+          {/* <Logon /> */}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Header
