eafb951bb2ada398f9355647f111d0a7198ddd59
ebordenave <edward.bordenave@gmail.com>
2026-06-10 05:33:49 -0400

    complete part 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,18 @@ import './App.css'
 import Logon from './features/Logon'
 import TodosPage from './features/Todos/TodosPage'
 import Header from './shared/Header'
-//! Use useAuth() to access isAuthenticated
-import useAuth from './hooks/useAuth'
+import { Outlet, Route, Routes } from 'react-router'
 
 function App() {
-  //! Remove authentication useState calls
-  // const [email, setEmail] = useState('')
-  // const [token, setToken] = useState('')
-
-  const { isAuthenticated } = useAuth()
-
-  //! Remove all authentication props from child components
   return (
     <>
       <Header />
-      {isAuthenticated ? <TodosPage /> : <Logon />}
+      {/* //! https://reactrouter.com/start/declarative/routing#layout-routes */}
+      <Routes>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/todos" element={<TodosPage />}></Route>
+        </Route>
+      </Routes>
     </>
   )
 }
